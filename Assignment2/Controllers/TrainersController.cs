3bcb303e021d0a5b7fed1d1a958ca178788c01ea
Theodore Stassinos <thestas@yahoo.com>
2022-07-03 17:35:13 +0300

    Add project files.

---
+++ Assignment2/Controllers/TrainersController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Assignment2;
+
+namespace Assignment2.Controllers
+{
+    public class TrainersController : Controller
+    {
+        private IndividualProjectBContext db = new IndividualProjectBContext();
+
+        // GET: Trainers
+        public ActionResult Index()
+        {
+            return View(db.Trainers.ToList());
+        }
+
+        // GET: Trainers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainer trainer = db.Trainers.Find(id);
+            if (trainer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainer);
+        }
+
+        // GET: Trainers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Trainers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "trainerId,firstName,lastName,subject")] Trainer trainer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Trainers.Add(trainer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(trainer);
+        }
+
+        // GET: Trainers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainer trainer = db.Trainers.Find(id);
+            if (trainer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainer);
+        }
+
+        // POST: Trainers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "trainerId,firstName,lastName,subject")] Trainer trainer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(trainer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(trainer);
+        }
+
+        // GET: Trainers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainer trainer = db.Trainers.Find(id);
+            if (trainer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainer);
+        }
+
+        // POST: Trainers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trainer trainer = db.Trainers.Find(id);
+            db.Trainers.Remove(trainer);
+            
+            //i put a rule in relationtship between trainersPerCourse and trainers table -> cascade on delete istead of this code
+            //List<trainersPerCourse> trainersPerCourseList = db.trainersPerCourses.Where(t => t.trainerId == id).ToList();
+            //foreach (var tpc in trainersPerCourseList)
+            //{
+            //    db.trainersPerCourses.Remove(tpc);
+            //}           
+            
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
