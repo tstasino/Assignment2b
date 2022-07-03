4d28bce872942e96080b9f755e1b7f4e06359faf
Theodore Stassinos <thestas@yahoo.com>
2022-07-03 17:37:30 +0300

    Added  a comment

--- Assignment2/Controllers/HomeController.cs
+++ Assignment2/Controllers/HomeController.cs
@@ -18,6 +18,8 @@ namespace Assignment2.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+            //test
+
         }
 
         public ActionResult Contact()
